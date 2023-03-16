428c6bf1c0b37791c2557651e709d10f81dc293b
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-03-16 14:55:36 +0100

    creation des pages

--- TODO/src/app/app.module.ts
+++ TODO/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AddComponent } from './pages/add/add.component';
+import { ListComponent } from './pages/list/list.component';
+import { HistoryComponent } from './pages/history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddComponent,
+    ListComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
