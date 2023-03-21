1f4ca059a7db81413d6deda89371ec46cd2e6703
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-03-21 23:39:14 +0100

    ajout formulaire

--- TODO/src/app/pages/add/add.component.ts
+++ TODO/src/app/pages/add/add.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ICategory } from 'src/app/todo';
 import { CATEGORYLIST } from 'src/app/todoList.mock';
 
@@ -10,22 +10,34 @@ import { CATEGORYLIST } from 'src/app/todoList.mock';
 })
 export class AddComponent {
 
-// Déclarer la propriété FormGroup
-listCategory : ICategory[] = CATEGORYLIST;
+  // Déclarer la propriété FormGroup
+  listCategory: ICategory[] = CATEGORYLIST;
+  todoForm!: FormGroup;
+  // validationError: [] = [];
 
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
-constructor(
-  public formBuilder : FormBuilder,
-) {}
+  ngOnInit() {
+    console.log("list", this.listCategory)
+    this.todoForm = this.formBuilder.group({
+      cat: [null, [Validators.required]],
+      todo: [null, [Validators.required]]
+    });
+    console.log(this.todoForm.value);
+  }
 
-ngOnInit(){
-  console.log("list", this.listCategory)
-}
-
-      
-
-
-
-
-
+  validate() {
+    // this.validationError = [];
+    if (this.todoForm.invalid) {
+      Object.keys(this.todoForm.controls).forEach((input) => {
+        const currentInput = this.todoForm.get(input);
+        console.log("currentInput", currentInput);
+        // if (currentInput && currentInput.status === "INVALID") {
+        //   this.validationError.push();
+        // }
+      });
+    }
+  }
 }
