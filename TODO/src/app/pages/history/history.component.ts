8c83dc283fda666d90c9b0da67ad22013b4d08c4
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-03-24 09:55:57 +0100

    mise en forme de la page history

--- TODO/src/app/pages/history/history.component.ts
+++ TODO/src/app/pages/history/history.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodosService } from 'src/app/services/todos.service';
+import { ITodo } from 'src/app/todo';
 
 @Component({
   selector: 'app-history',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./history.component.css']
 })
 export class HistoryComponent {
+  todoList: ITodo[] = [];
+
+
+constructor(
+  public todoService: TodosService
+) { }
+
+ngOnInit(): void {
+    this.getTodoList();
+}
+
+getTodoList(){
+this.todoList = this.todoService.getTodoList();
+}
 
 }
+
+
+
+
