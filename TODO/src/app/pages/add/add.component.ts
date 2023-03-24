644d59b6344f3ec6927cc16ff2dc6666bf148c03
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-03-24 11:00:18 +0100

    ajout de la date

--- TODO/src/app/pages/add/add.component.ts
+++ TODO/src/app/pages/add/add.component.ts
@@ -15,17 +15,17 @@ import { ITodo } from 'src/app/todo';
 export class AddComponent {
 
   // Déclarer la propriété FormGroup
-  
+
   todoForm!: FormGroup;
   validationError: [] = [];
   todoList: ITodo[] = [];
-  todoItem : ITodo = {
-    id : 0,
-    content : "",
+  todoItem: ITodo = {
+    id: 0,
+    content: "",
     category: null,
     picture: "",
     isUrgent: false,
-    doneDate: null
+    doneDate: new Date
   }
 
   constructor(
@@ -36,39 +36,48 @@ export class AddComponent {
   }
 
   ngOnInit() {
-    
+
     this.todoForm = this.formBuilder.group({
       cat: [null],
       todo: [null, [Validators.required]],
-      urgence:[false]
+      urgence: [false]
     });
     this.getTodoList();
-  console.log("todolist pour test",this.todoList);
+    console.log("todolist pour test", this.todoList);
   }
 
- 
 
-  addTodo() {
-    //j'assigne la valeur de la catégorie à l'attribut category de ma todo
-    this.todoItem.category = this.todoForm.value.cat;
-     //j'assigne la valeur de la todo à l'attribut content de ma todo
-    this.todoItem.content = this.todoForm.value.todo;
-     //j'assigne l'urgence à l'attribut category de ma todo
-    this.todoItem.isUrgent = this.todoForm.value.urgence;
-    
-   //j'ajoute les éléments au todoItem
-    this.todoService.addToTodoList(this.todoItem);
-    // je redirige sur la page home
-    this.router.navigate(['home']);
-    
-  
+  //fonction pour générer un id
+  genId(todoList: ITodo[]): number {
+    return todoList.length > 0 ? Math.max(...todoList.map(todoItem => todoItem.id)) + 1 : 1;
   }
 
+
+  //fonction pour récupérer ma todo
   getTodoList() {
     this.todoList = this.todoService.getTodoList();
   }
- 
 
+  
+  //créer une fonction pour donner un id à une todo
+  addTodo() {
+    //je récupère ma todoList
+    const todoList = this.getTodoList();
+    //je lui donne un id
+    // this.todoItem.id = this.genId(todoList);
+    //j'assigne la valeur de la catégorie à l'attribut category de ma todo
+    this.todoItem.category = this.todoForm.value.cat;
+    //j'assigne la valeur de la todo à l'attribut content de ma todo
+    this.todoItem.content = this.todoForm.value.todo;
+    //j'assigne l'urgence à l'attribut category de ma todo
+    this.todoItem.isUrgent = this.todoForm.value.urgence;
+    // ajout de la date à null pour le moment car todo en cours
+    this.todoItem.doneDate = null;
+    //j'ajoute les éléments au todoItem
+    this.todoService.addToTodoList(this.todoItem);
+    // je redirige sur la page home
+    this.router.navigate(['home']);
+  }
 
 
 
