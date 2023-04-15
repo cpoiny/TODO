1ec4d0e836bfc7f4bd14a983551b82b7610b324e
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-04-15 15:21:40 +0200

    ajout de la fonction addTodo quand todo édité, manque la suppression dans la liste normale)

--- TODO/src/app/components/todo-card/todo-card.component.ts
+++ TODO/src/app/components/todo-card/todo-card.component.ts
@@ -12,6 +12,14 @@ import { ITodo } from 'src/app/todo';
 export class TodoCardComponent {
 
   todoDetails?: ITodo;
+  // todoItem: ITodo = {
+  //   id: this.todoDetails!.id,
+  //   content: "",
+  //   category: null,
+  //   picture: "",
+  //   isUrgent: false,
+  //   doneDate: null
+  // }
 
   constructor(
     public todosService: TodosService,
@@ -63,6 +71,45 @@ export class TodoCardComponent {
       todo: this.todoDetails.content,
       urgence: this.todoDetails.isUrgent,
     });
+  }
+
+
+
+  //créer une fonction pour donner un id à une todo
+  addTodo(): void {
+    console.log("etape1");
+    //j'assigne la valeur de la catégorie à l'attribut category de ma todo
+    this.todoDetails!.category = this.todoForm.value.cat;
+    //j'assigne la valeur de la todo à l'attribut content de ma todo
+    this.todoDetails!.content = this.todoForm.value.todo;
+    //j'assigne l'urgence à l'attribut category de ma todo
+    if (this.todoForm.value.urgence) {
+      this.todoDetails!.isUrgent = true;
+      console.log("urgence dans fonction", this.todoForm.value.urgence);
+      console.log("urgence dans fonction", this.todoDetails!.isUrgent);
+    };
+
+    //j'ajoute les éléments au todoItem
+    this.todosService.addToTodoList(this.todoDetails!);
+    console.log ("2");
+
+    //recuperer la liste totale
+    const todoList = this.todosService.getTodoList();
+
+    //je supprime la todo dans history si elle était dans history
+    if (this.todoDetails?.doneDate !== null) {
+      const historyList = this.todosService.getHistoryList();
+      historyList.splice(this.todoDetails!.id, 1);
+      this.todosService.saveTodoList(historyList);
+    } else {
+      const filtratedList = todoList.filter((todo: ITodo) => todo.doneDate === null);
+      filtratedList.splice(this.todoDetails.id, 1);
+      this.todosService.saveTodoList(filtratedList);
+    }
+    console.log("nouvelle tache", this.todoDetails!);
+    // je redirige sur la page home
+    this.router.navigate(['home']);
+
   }
 }
 
